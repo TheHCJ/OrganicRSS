8d295805ab291a6ad676f175041f2b7981a70e1f
HCJ <150591975+TheHCJ@users.noreply.github.com>
2024-10-30 20:21:22 +0000

    add basic skeleton for the homescreen

--- lib/skeleton/homeScreen.dart
+++ lib/skeleton/homeScreen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
+import 'package:organicrss/rss/parseRSS.dart';
+import 'package:organicrss/rss/rssClasses.dart';
+
 class homeScreen extends StatefulWidget {
   State<homeScreen> createState() => homeScreenState();
 }
 
 class homeScreenState extends State<homeScreen> {
+  final feedUri = "https://feeds.bbci.co.uk/news/rss.xml";
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Home"),),
+        appBar: AppBar(title: const Text("Home"),),
+        body: FutureBuilder(future: parseRSS(feedUri: feedUri).parse(), builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LinearProgressIndicator();
+          } else {
+            final items = snapshot.data;
+            if (items != null)
+            {
+            return ListView.builder(itemBuilder: (BuildContext context, int i) {
+              final rssItem item = items[i];
+              return ListTile(title: Text(item.title), subtitle: Text(item.description),);
+            });
+          } else {
+            return const LinearProgressIndicator();
+          }
+          }
+        })
       );
   }
 }
