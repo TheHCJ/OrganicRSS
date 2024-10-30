87d700b1d2efb91d122c6c5108fc737059640a8d
HCJ <150591975+TheHCJ@users.noreply.github.com>
2024-10-30 20:39:29 +0000

    tap item to open url

--- lib/skeleton/homeScreen.dart
+++ lib/skeleton/homeScreen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:organicrss/rss/parseRSS.dart';
 import 'package:organicrss/rss/rssClasses.dart';
 
+import 'package:url_launcher/url_launcher_string.dart';
+
 class homeScreen extends StatefulWidget {
   State<homeScreen> createState() => homeScreenState();
 }
@@ -22,7 +24,7 @@ class homeScreenState extends State<homeScreen> {
             {
             return ListView.builder(itemBuilder: (BuildContext context, int i) {
               final rssItem item = items[i];
-              return ListTile(title: Text(item.title), subtitle: Text(item.description),);
+              return ListTile(title: Text(item.title), subtitle: Text(item.description), onTap: () => launchUrlString(item.uri),);
             });
           } else {
             return const LinearProgressIndicator();
